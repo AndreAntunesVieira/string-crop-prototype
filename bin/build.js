6840dfdacbc28697bdcdcc723d2be00efccaaa23
Andre Antunes Vieira <andreantunesv@gmail.com>
2018-12-02 12:38:38 -0200

    Optimize build (mangle)

--- bin/build.js
+++ bin/build.js
@@ -3,5 +3,5 @@ const uglifyJS = require('uglify-js-es6')
 
 fs.readFile('src/index.js', 'utf8')
   .then(content => fs.writeFile('dist/index.js', content))
-  .then(() => uglifyJS.minify(['dist/index.js'], { compress: { dead_code: true, global_defs: { DEBUG: false } } }).code)
+  .then(() => uglifyJS.minify(['dist/index.js'], { compress: { dead_code: true, global_defs: { DEBUG: false, PRODUCTION: true } } }).code)
   .then(content => fs.writeFile('dist/index.js', content))
